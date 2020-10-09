70808c190278d717804eed5086e2ec838a202663
Dmitrii Zheleztsov <soil7800@gmail.com>
2020-10-09 18:09:36 +0300

    added score

--- snake.py
+++ snake.py
@@ -1,18 +1,27 @@
 import pygame
 import random
 
+pygame.init()
 COL = 19
 ROW = 19
-GRID_CELL = 40
-RESOLUTION = COL * GRID_CELL, ROW * GRID_CELL
+BACKGROUND_CELL = 40
+GRID_CELL = BACKGROUND_CELL - 2
+RESOLUTION = COL * BACKGROUND_CELL, ROW * BACKGROUND_CELL + 40
+GAME_FIELD_RES = COL * BACKGROUND_CELL, ROW * BACKGROUND_CELL
+INFO_RES = COL * BACKGROUND_CELL, 40
 FPS = 8
+BACKGROUND = []
 GRID = []
 AVIABLE_COORDS = set()
+FONT = pygame.font.Font(None, 30)
 for x in range(COL):
+    BACKGROUND.append([])
     GRID.append([])
     for y in range(ROW):
-        GRID[x].append(pygame.Rect(x * GRID_CELL, y * GRID_CELL, GRID_CELL, GRID_CELL))
+        BACKGROUND[x].append(pygame.Rect(x * BACKGROUND_CELL, y * BACKGROUND_CELL, BACKGROUND_CELL, BACKGROUND_CELL))
+        GRID[x].append(pygame.Rect(x * BACKGROUND_CELL + 1, y * BACKGROUND_CELL + 1, GRID_CELL, GRID_CELL))
         AVIABLE_COORDS.add((x, y))
+
 
 class Snake:
     color = (76, 122, 244)
@@ -23,6 +32,7 @@ class Snake:
         self.dy = 0
         self.head_x = 2
         self.head_y = 9
+        self.score = 0
     
     def move(self, apple):
         self.head_x += self.dx
@@ -31,6 +41,7 @@ class Snake:
             self.__init__()
             apple.__init__()
         elif GRID[self.head_x][self.head_y].colliderect(GRID[apple.x][apple.y]):
+            self.score += 1
             self.parts.append((self.head_x, self.head_y))
             apple.aviable_coords.discard((self.head_x, self.head_y))
             apple.change_coords()
@@ -50,7 +61,9 @@ class Snake:
         elif event.key == pygame.K_DOWN and self.dy != -1: 
             self.dx , self.dy  = 0, 1
 
-    def draw(self, scr):
+    def draw(self, scr, info):
+        current_score = FONT.render(f'score: {self.score}', 1, (255, 255, 255))
+        info.blit(current_score, (5, 5))
         for x, y in self.parts:
             pygame.draw.rect(scr, self.color, GRID[x][y])
 
@@ -66,36 +79,38 @@ class Apple:
     
     def change_coords(self):
         self.x, self.y = self.aviable_coords.pop()
-        print(len(self.aviable_coords))
 
 
 if __name__ == "__main__":
-    pygame.init()
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode(RESOLUTION)
+    game_field = pygame.Surface(GAME_FIELD_RES)
+    info = pygame.Surface(INFO_RES)
     apple = Apple()
     snake = Snake()
-    
+    screen.blit(info, (0, 0))
+    screen.blit(game_field, (0, 0))
     # main cycle
     while True:
-        screen.fill(pygame.Color('black'))
-        
+        screen.blit(info, (0, 0))
+        info.fill((72, 116, 41))
+        screen.blit(game_field, (0, 40))
         for x in range(COL):
             for y in range(ROW):
                 if x % 2 != 0:
                     if y % 2 != 0:
-                        pygame.draw.rect(screen, (171, 214, 81), GRID[x][y])
+                        pygame.draw.rect(game_field, (171, 214, 81), BACKGROUND[x][y])
                     else:
-                        pygame.draw.rect(screen, (161, 207, 72), GRID[x][y])
+                        pygame.draw.rect(game_field, (161, 207, 72), BACKGROUND[x][y])
                 else:
                     if y % 2 == 0:
-                        pygame.draw.rect(screen, (171, 214, 81), GRID[x][y])
+                        pygame.draw.rect(game_field, (171, 214, 81), BACKGROUND[x][y])
                     else:
-                        pygame.draw.rect(screen, (161, 207, 72), GRID[x][y])
+                        pygame.draw.rect(game_field, (161, 207, 72), BACKGROUND[x][y])
                 
-        apple.draw(screen)
+        apple.draw(game_field)
         snake.move(apple)
-        snake.draw(screen)
+        snake.draw(game_field, info)
         pygame.display.update()
         clock.tick(FPS)
         # check event list
